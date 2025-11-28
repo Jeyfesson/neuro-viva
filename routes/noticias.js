5c5f0bb734da79809db4824ef01491562311a161
Jeyfesson <jeyfersson.souza@gmail.com>
2025-11-28 13:30:34 +0000

    refactor: substituir NoticiaController por InformacaoController nas rotas

--- routes/noticias.js
+++ routes/noticias.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const routerPublic = express.Router();
 const routerAdmin = express.Router();
-const NoticiaController = require("../controllers/NoticiaController");
+const InformacaoController = require("../controllers/InformacaoController");
 const asyncHandler = require("../middleware/asyncHandler");
 
 // Public routes
-routerPublic.get("/", asyncHandler(NoticiaController.indexPublic));
-routerPublic.get("/noticia/:id", asyncHandler(NoticiaController.viewNoticia));
+routerPublic.get("/", asyncHandler(InformacaoController.indexPublic));
+routerPublic.get("/noticia/:id", asyncHandler(InformacaoController.viewNoticia));
 routerPublic.get(
   "/categoria/:id",
-  asyncHandler(NoticiaController.listarPorCategoria)
+  asyncHandler(InformacaoController.listarPorCategoria)
 );
 
 // Admin routes (these will be mounted under /admin/noticias)
-routerAdmin.get("/", asyncHandler(NoticiaController.listarAdmin));
-routerAdmin.get("/nova", asyncHandler(NoticiaController.novaForm));
-routerAdmin.post("/nova", asyncHandler(NoticiaController.criar));
-routerAdmin.get("/editar/:id", asyncHandler(NoticiaController.editarForm));
-routerAdmin.post("/editar/:id", asyncHandler(NoticiaController.atualizar));
-routerAdmin.post("/deletar/:id", asyncHandler(NoticiaController.deletar));
+routerAdmin.get("/", asyncHandler(InformacaoController.listarAdmin));
+routerAdmin.get("/nova", asyncHandler(InformacaoController.novaForm));
+routerAdmin.post("/nova", asyncHandler(InformacaoController.criar));
+routerAdmin.get("/editar/:id", asyncHandler(InformacaoController.editarForm));
+routerAdmin.post("/editar/:id", asyncHandler(InformacaoController.atualizar));
+routerAdmin.post("/deletar/:id", asyncHandler(InformacaoController.deletar));
 
 module.exports = { public: routerPublic, admin: routerAdmin };
