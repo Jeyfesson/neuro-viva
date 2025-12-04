092fea16cfb8d78e4016987e04f47b4f19329272
Jeyfesson <jeyfersson.souza@gmail.com>
2025-12-04 21:08:02 +0000

    fix(db): remover comentário desnecessário sobre criação de tabela

--- config/db.js
+++ config/db.js
@@ -8,7 +8,6 @@ const db = new sqlite3.Database(dbPath, (err) => {
   else console.log("✅ Banco SQLite conectado:", dbPath);
 });
 
-// cria tabela se não existir
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS usuarios (
